f7a02d0a20432ac1e31602df7a54ffb35fc4ba5f
jpreslov <lovett_jp@outlook.com>
2021-01-31 11:34:26 -0600

    personal store for screen printing

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+
+import logo from "../../assets/ren_logo.PNG";
+import useStyles from "./styles";
+import { Link, useLocation } from "react-router-dom";
+
+const Navbar = () => {
+  const classes = useStyles();
+  const location = useLocation();
+
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography
+            component={Link}
+            to="/"
+            variant="h6"
+            className={classes.title}
+            color="inherit"
+          >
+            <img
+              src={logo}
+              alt="Commerce.js"
+              height="90vh"
+              className={classes.image}
+            />
+            {/* Rendezvous PG */}
+          </Typography>
+          <div className={classes.grow} />
+          {location.pathname === "/" && (
+            <div className={classes.button}>
+              <IconButton
+                component={Link}
+                to="/cart"
+                aria-label="View cart"
+                color="inherit"
+              >
+                <Badge badgeContent={2} color="secondary">
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
