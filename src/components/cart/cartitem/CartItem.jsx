972357eeb9f84ed20b88023b46b53051ff703d18
Jake Lovett <jpreslov@jakes-mbp.home>
2021-04-20 19:42:44 -0500

    safari bs

--- src/components/cart/cartitem/CartItem.jsx
+++ src/components/cart/cartitem/CartItem.jsx
@@ -1,36 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import { Typography, Button, Card, CardActions, CardContent, CardMedia, Input } from '@material-ui/core';
-import HighlightOffIcon from '@material-ui/icons/HighlightOff';
-import useStyles from './styles';
+import React, { useEffect, useState } from "react";
+import { Typography, Button, Card, CardActions, CardContent, CardMedia, Input } from "@material-ui/core";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
+import useStyles from "./styles";
+import { isSafari } from "react-device-detect";
+
+import './cartItemStyles.scss'
 
 const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
   // const [ qty, setQty ] = useState('')
 
   const classes = useStyles();
-  
   const handleUpdateCartQty = (lineItemId, newQuantity) => onUpdateCartQty(lineItemId, newQuantity);
-
   const handleRemoveFromCart = (lineItemId) => onRemoveFromCart(lineItemId);
 
-  return item.selected_options.map((selected_option) => {
-    return (
-      <Card className={classes.cartItem}>
-        <HighlightOffIcon className={classes.removeButton} onClick={() => handleRemoveFromCart(item.id)}/>
-        <CardMedia image={item.media.source} alt={item.name} className={classes.media} />
-        <CardContent className={classes.cardContent}>
-            <Typography className={classes.text}>{item.name}</Typography>
-            <Typography variant="subtitle2">
+  if (isSafari) {
+    return item.selected_options.map((selected_option) => {
+      return (
+        <div className="cart-item">
+          <HighlightOffIcon className="remove-button" onClick={() => handleRemoveFromCart(item.id)} />
+          <img src={item.media.source} alt={item.name} className={classes.media} />
+          <div className={classes.div}>
+            <h5 className="item-name">{item.name} asldkfjhas;odifhjas;ldifha</h5>
+            <h6 className="variant-info">
               {selected_option.group_name}: {selected_option.option_name}
-            </Typography>
-          <Typography variant="subtitle1">{item.line_total.formatted_with_symbol}</Typography>
-        </CardContent>
-        <CardActions className={classes.cardActions}>
-            <Input className={classes.qtyInput} defaultValue={item.quantity} onChange={(e) => handleUpdateCartQty(item.id, e.target.value)}/>
-         
-        </CardActions>
-      </Card>
-    );
-  });
+            </h6>
+            <p className="line-total">{item.line_total.formatted_with_symbol}</p>
+          </div>
+          <div className={classes.cardActions}>
+            <Input className={classes.qtyInput} defaultValue={item.quantity} onChange={(e) => handleUpdateCartQty(item.id, e.target.value)} />
+          </div>
+        </div>
+      );
+    });
+  } else {
+    return item.selected_options.map((selected_option) => {
+      return <h1 style={{ marginTop: "30vh" }}>Not Safari</h1>;
+    });
+  }
 };
 
 export default CartItem;
